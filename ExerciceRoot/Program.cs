033660a0250741bc3c789f9ebcb01a574025aa40
Eya <Eya@EYA-LENOVO-PC>
2024-10-22 14:11:23 +0200

    Add project files.

---
+++ ExerciceRoot/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+
+    public static void CalculeRacine(double number, int root)
+    {
+        if (number >= 1.00 && root >= 1)
+        {
+            double result = 1;
+            double nth = 1;
+            while (true)
+            {
+                result = 1;
+                for (int i = 0; i < root; i++)
+                {
+                    result *= nth;
+                }
+                if (result >= number)
+                    break;
+                else
+                    nth += 0.0001;
+            }
+            Console.WriteLine($"valeur approximative {nth}  ≈  {(int)nth}");
+
+        }
+        else
+        {
+            Console.WriteLine("Erreur Parametre");
+        }
+
+    }
+    private static void Main(string[] args)
+    {
+        CalculeRacine(16, 4);
+        CalculeRacine(8, 3);
+        CalculeRacine(10, 2);
+        CalculeRacine(8, 3);
+        CalculeRacine(1, 1);
+        CalculeRacine(25, 2);
+        CalculeRacine(81, 4);
+    }
+}
